bfcb3e433b7fb907d62a8fdbc00812f7f6a984ff
Hari Dandapani <haridandapani@gmail.com>
2022-02-28 15:49:33 -0500

    Adding potential code for excel_opener

---
+++ code/comparison.py
@@ -0,0 +1,28 @@
+def generateName(name):
+    return name
+
+def generateCategoryFromCategoryOne(category):
+    return category
+
+def generateListFromHours(hours):
+    return hours.split(";")
+
+def compareHours(hours_1, hours_2):
+    def intersection(lst1, lst2):
+        lst3 = [value for value in lst1 if value in lst2]
+        return lst3
+    overlap = intersection(hours_1, hours_2)
+    return len(overlap)
+
+def getParserDict():
+    parser_dict = {"hours": generateListFromHours, "category_one": generateCategoryFromCategoryOne, "name": generateName}
+    return parser_dict
+
+def runner():
+    hours_1 = "10 AM - 11 AM;11 AM - 12 PM;12 PM - 1 PM"
+    hours_2 = "11 AM - 12 PM;12 PM - 1 PM;2 PM - 3 PM;3 PM - 4 PM"
+    parser_dict = getParserDict()
+    comp_hours_1 = parser_dict["hours"](hours_1)
+    comp_hours_2 = parser_dict["hours"](hours_2)
+    comparison = compareHours(comp_hours_1, comp_hours_2)
+    print(comparison)
